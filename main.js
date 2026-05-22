f5fa2b2ff3cf27b9dbd187056aee2c06516f4841
dev-rfgul <rfgul587@gmail.com>
2026-05-22 14:15:59 +0500

    feat: add health check endpoint for CI/CD pipeline

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT ?? 8080;
 app.get("/", (req, res) => {
   res.json({ message: "Hello World,I am a Node.js server! and this is a simple API" });
 });
+app.get("/health/ci-cd",(req,res)=>{
+  res.json({message:"CI/CD pipeline is working fine!"})
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
